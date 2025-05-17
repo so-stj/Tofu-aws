cf7eac88266c280e66d239e8df3f0575e65ccf1d
Sei <hazen.60.t@gmail.com>
2025-05-17 23:28:51 +0200

    add

---
+++ iam.tf
@@ -0,0 +1,34 @@
+resource "aws_iam_openid_connect_provider" "terraform_cicd" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
+
+resource "aws_iam_role" "terraform_cicd_oidc_role" {
+  name = "TerraCICDDemoOIDCRole"
+  path = "/"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Principal = {
+        Federated = aws_iam_openid_connect_provider.terraform_cicd.arn
+      }
+      Condition = {
+        StringLike = {
+          "token.actions.githubusercontent.com:sub" = [
+            "repo:so-stj/Tofu-aws:*",
+          ]
+        }
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "AdministratorAccess_attachment" {
+  role       = aws_iam_role.terraform_cicd_oidc_role.name
+  # Admin権限を指定
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
---
+++ main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.0.0-beta1"
+    }
+  }
+}
+
+provider "aws" {
+    region  = "ap-northeast-1"
+  # Configuration options
+}
