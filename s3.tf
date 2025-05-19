07ef151f33096a054304df8ab6ac959be78f64ed
Sei <hazen.60.t@gmail.com>
2025-05-19 02:54:54 +0200

    changes

--- s3.tf
+++ s3.tf
@@ -1,7 +1,7 @@
 # S3バケットの定義
 resource "aws_s3_bucket" "terraform_state" {
   # バケット名は一意の必要があるので、bucketの値は各自変更してください
-  bucket = "opentofu-state-bucket"
+  bucket = "tf-state-bucket-devops-23"
 }
 
 # バージョニング設定
