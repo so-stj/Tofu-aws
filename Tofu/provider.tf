b08e7404ecb85424f5c697c36183c251b6d06c6f
Sei <hazen.60.t@gmail.com>
2025-05-30 22:02:20 +0200

    changes

--- Tofu/provider.tf
+++ Tofu/provider.tf
@@ -4,16 +4,16 @@ terraform {
 
   required_providers {
     aws = {
-      source = "opentofu/aws"
+      source  = "opentofu/aws"
       version = "6.0.0-beta1"
     }
   }
-   backend "s3" {
-   bucket = "tf-state-bucket-devops-23"
-   region = "us-east-1"
-   key = "test/terraform.tfstate"
-   encrypt = true
- }
+  backend "s3" {
+    bucket  = "tf-state-bucket-devops-23"
+    region  = "us-east-1"
+    key     = "test/terraform.tfstate"
+    encrypt = true
+  }
 }
 
 provider "aws" {
